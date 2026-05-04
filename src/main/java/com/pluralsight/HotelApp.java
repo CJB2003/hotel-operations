8381f24bbf609e73c466b833df4880a25aaa4308
chrisbunny <christopherjbunny@gmail.com>
2026-05-04 15:25:19 -0500

    Finished

--- src/main/java/com/pluralsight/HotelApp.java
+++ src/main/java/com/pluralsight/HotelApp.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
 public class HotelApp {
+    public static void main(String[] args) {
+
+        Room room1 = new Room(2, 99, true, true);
+
+        Reservation reservation1 = new Reservation("king", 5, true);
+
+        Employee employee1 = new Employee(1, "Chris", "Customer Service", 21.55, 9);
+
+        System.out.println("Is room 1 available? " + room1.isAvailable());
+
+        System.out.printf("The total price of your reservation is: $%.2f\n", reservation1.getReservationTotal());
+
+
+        System.out.printf("Your pay for this week: $%.2f", employee1.getTotalPay());
+    }
 }
--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
 public class Reservation {
+    private String roomType;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        double price = 0;
+        if (roomType.equalsIgnoreCase("King")) {
+            price = 139.00;
+        }
+        else if(roomType.equalsIgnoreCase("Double")) {
+            price = 124.00;
+        }
+        return price;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    public double getReservationTotal() {
+        double price = getPrice();
+        if (isWeekend) {
+            price = price * 0.1 + price;
+            price += numberOfNights;
+        }
+        return price;
+    }
 }
